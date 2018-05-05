05ffcf807e3934ffbdfe5273d0a6f263b1dbd6fd
Krystian Fira <Rasdersh@wp.pl>
2018-05-05 09:56:34 +0200

    Version 0.3

--- js/scripts.js
+++ js/scripts.js
@@ -13,6 +13,14 @@ var calculationController = (function() {
         this.value = value;
     };
 
+    var calculateTotal = function(type) {
+        var sum = 0;
+        data.allItems[type].forEach(function(cur) {
+            sum += cur.value;
+        });
+        data.totals[type] = sum;
+    };
+
     // Data structure
     var data = {
         allItems: {
@@ -22,7 +30,9 @@ var calculationController = (function() {
         totals: {
             exp: 0,
             inc: 0
-        }
+        },
+        budget: 0,
+        percentage: -1
     };
 
     return {
@@ -50,6 +60,32 @@ var calculationController = (function() {
             return newItem;
         },
 
+        calculateBudget: function() {
+
+            // Calculate total income and expenses 
+            calculateTotal("exp");
+            calculateTotal("inc");
+
+            // Calculate the budget: income - expenses
+            data.budget = data.totals.inc - data.totals.exp;
+
+            // Calculate the percentage of income that we spent
+            if(data.totals.inc > 0) {
+                data.percentage = Math.round((data.totals.exp / data.totals.inc) * 100);
+            } else {
+                data.percentage = -1;
+            }
+        },
+
+        getBudget: function() {
+            return {
+                budget: data.budget,
+                totalInc: data.totals.inc,
+                totalExp: data.totals.exp,
+                percentage: data.percentage
+            };
+        },
+
         testing: function() {
             console.log(data);
         }
@@ -65,8 +101,12 @@ var userInterfaceController = (function() {
         inputDescription: ".insert__description",
         inputValue: ".insert__value",
         inputButton: ".insert__btn",
-        profitContainer: '.income__list',
-        lossContainer: '.expenses__list'
+        profitContainer: ".income__list",
+        lossContainer: ".expenses__list",
+        budgetLabel: ".converter__value",
+        incomeLabel: ".converter__income--value",
+        expensesLabel: ".converter__expenses--value",
+        percentageLabel: ".converter__expenses--percentage"
     };
 
     return {
@@ -74,7 +114,7 @@ var userInterfaceController = (function() {
             return {
                 type: document.querySelector(DOMStrings.inputType).value, // Profit or loss
                 description: document.querySelector(DOMStrings.inputDescription).value,
-                value: document.querySelector(DOMStrings.inputValue).value
+                value: parseFloat(document.querySelector(DOMStrings.inputValue).value)
             };
         },
 
@@ -88,7 +128,7 @@ var userInterfaceController = (function() {
 
                 html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
             } else if(type === "exp") {
-                element.DOMStrings.lossContainer;
+                element = DOMStrings.lossContainer;
 
                 html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
             }
@@ -101,6 +141,32 @@ var userInterfaceController = (function() {
             // Insert the HTML into the DOM 
             document.querySelector(element).insertAdjacentHTML("beforeend", newHtml);
 
+        },
+
+        clearFields: function() {
+            var fields, fieldsArray;
+
+            fields = document.querySelectorAll(DOMStrings.inputDescription + ", " + DOMStrings.inputValue);
+
+            fieldsArray = Array.prototype.slice.call(fields);
+
+            fieldsArray.forEach(function(current, index, array) {
+                current.value = "";
+            });
+
+            fieldsArray[0].focus();
+        },
+
+        displayBudget: function(obj) {
+            document.querySelector(DOMStrings.budgetLabel).textContent = obj.budget;
+            document.querySelector(DOMStrings.incomeLabel).textContent = obj.totalInc;
+            document.querySelector(DOMStrings.expensesLabel).textContent = obj.totalExp;
+
+            if(obj.percentage > 0) {
+                document.querySelector(DOMStrings.percentageLabel).textContent = obj.percentage + "%";
+            } else {
+                document.querySelector(DOMStrings.percentageLabel).textContent = "---";
+            }
         },
 
         getDOMStrings: function() {
@@ -129,26 +195,49 @@ var applicationController = (function(calculationCtrl, userInterfaceCtrl) {
         });
     }
 
+    var updateCalculationController = function() {
+        
+        // Calculate the budget
+        calculationCtrl.calculateBudget();
+
+        // Return the budget
+        var budget = calculationCtrl.getBudget();
+
+        // Display the budget on user interface controller
+        userInterfaceController.displayBudget(budget);
+    };
+
     var ctrlAddItem = function() {
         var input, newItem;
 
         // Get the field input data
         input = userInterfaceCtrl.getInput();
 
-        // Add the item to the calculation controller
-        newItem = calculationController.addItem(input.type, input.description, input.value);
+        if(input.description !== "" && !isNaN(input.value) && input.value > 0) {
+            
+            // Add the item to the calculation controller
+            newItem = calculationController.addItem(input.type, input.description, input.value);
 
-        // Add the item to the user interface controller
-        userInterfaceController.addListItem(newItem, input.type);
+            // Add the item to the user interface controller
+            userInterfaceController.addListItem(newItem, input.type);
 
-        // Calculate the budget
+            // Clear the fields
+            userInterfaceController.clearFields();
 
-        // Display the budget on user interface controller
+            // Calculate and update budget
+            updateCalculationController();
+        }
     };
 
     return {
         init: function() {
             console.log("Its works!");
+            userInterfaceController.displayBudget({
+                budget: 0,
+                totalInc: 0,
+                totalExp: 0,
+                percentage: -1
+            });
             eventListenersConfiguration();
         }
     };
